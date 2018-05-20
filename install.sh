85cbada00ab989208b3c58107fc649a1d43eb2d5
buptweixin <weixin@bupt.edu.cn>
2018-05-20 19:24:08 +0800

    init project

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ sudo usermod -s /bin/zsh $(whoami)
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+
+# zshrc
+ln -s -f zshrc ~/.zshrc
+
 # tmux
 ln -s -f tmux/.tmux.conf ~/.tmux.conf
 cp tmux/.tmux.conf.local ~/.tmux.conf.local
